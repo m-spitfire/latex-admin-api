be83a73579b30e1125e86632f06a2184bd4dd883
spitfire-hash <carlsonmu@gmail.com>
2020-11-17 14:27:41 +0400

    added landing page

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,14 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path(f"{settings.ADMIN_URL}doc/", include('django.contrib.admindocs.urls')),
     path(settings.ADMIN_URL, admin.site.urls),
+    path('', TemplateView.as_view(template_name="pages/index.html"), name="home"),
     # api urls
     path("api/", include("config.api_router")),
     path("auth-token/", obtain_auth_token)
